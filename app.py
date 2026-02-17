264d57f28e69a000ffacf21455966c07c0fe32e9
mdsy5444-gif <m.d.s.y5444@gmail.com>
2026-02-17 12:34:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,11 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
-import json
+from flask import Flask
 
 app = Flask(__name__)
 
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
-LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
-
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-MEMORY_FILE = "memory.json"
-
-def load_memory():
-    try:
-        with open(MEMORY_FILE, "r") as f:
-            return json.load(f)
-    except:
-        return {}
-
-def save_memory(data):
-    with open(MEMORY_FILE, "w") as f:
-        json.dump(data, f)
-
-@app.route("/callback", methods=["POST"])
-def callback():
-    signature = request.headers.get("X-Line-Signature")
-    body = request.get_data(as_text=True)
-
-    try:
-        handler.handle(body, signature)
-    except:
-        abort(400)
-
-    return "OK"
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    user_id = event.source.user_id
-    text = event.message.text
-
-    memory = load_memory()
-
-    if user_id not in memory:
-        memory[user_id] = []
-
-    # 覚える
-    if text.startswith("覚えて"):
-        content = text.replace("覚えて", "").strip()
-        memory[user_id].append(content)
-        save_memory(memory)
-        reply = f"覚えたよ！『{content}』"
-    
-    # 思い出す
-    elif text == "思い出して":
-        if memory[user_id]:
-            reply = "覚えていること:\n" + "\n".join(memory[user_id])
-        else:
-            reply = "まだ何も覚えてないよ"
-    
-    # 通常会話
-    else:
-        reply = "わからないな？"
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply)
-    )
+@app.route("/")
+def home():
+    return "Railway is working!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run()
+    
