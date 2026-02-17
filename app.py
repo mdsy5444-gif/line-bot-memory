084c0d858195815dd16b7bd32c17800ce5ee3278
mdsy5444-gif <m.d.s.y5444@gmail.com>
2026-02-17 12:43:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request, abort
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Railway is working!"
+    return "鉄道は動いてます"
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    # 今はとりあえず受け取ってすぐ返すだけ
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run()
-    
